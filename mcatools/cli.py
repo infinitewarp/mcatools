ecbb0d393ea5e998b6f2129d646cb6ba56ac40d8
Brad Smith <bradster@infinitewarp.com>
2018-12-09 15:28:34 -0500

    add import_biome cli command

--- mcatools/cli.py
+++ mcatools/cli.py
@@ -1,6 +1,9 @@
+import shutil
+import time
+
 import click
 
-from mcatools.imageutils import save_biome_image_rgb
+from mcatools.imageutils import save_biome_image_rgb, load_biome_image_rgb
 from mcatools.region import Region
 
 
@@ -20,4 +23,20 @@ def export_biome(region_filename, image_filename):
     click.echo(f"Biome data saved to: {image_filename}")
 
 
+@click.command()
+@click.argument("region_filename")
+@click.argument("image_filename")
+def import_biome(region_filename, image_filename):
+    click.echo(f"Loading region data from: {region_filename}")
+    region = Region.load(region_filename)
+    biome_data = load_biome_image_rgb(image_filename)
+    region.set_biomes(biome_data)
+    region_filename_backup = f"{region_filename}-{time.time()}"
+    click.echo(f"Backing up {region_filename} to {region_filename_backup}")
+    shutil.copy2(region_filename, region_filename_backup)
+    region.save(region_filename)
+    click.echo(f"Biome data saved to region: {region_filename}")
+
+
 main.add_command(export_biome)
+main.add_command(import_biome)
